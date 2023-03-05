d15b581aead31dbc2a6eb118ac61974eb3c0bd54
juan pablo bustamante <bustamante.jpm@gmail.com>
2023-03-05 15:29:13 -0300

    fix

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 function promiseAll(promises) {
     return new Promise((resolve, reject) => {
-      let results = [];
-      let completed = 0;
-  
-      promises.forEach((promise, index) => {
-        promise.then((result) => {
-          results[index] = result;
-          completed++;
-  
-          if (completed === promises.length) {
-            resolve(results);
-          }
-        }).catch((error) => {
-          reject(error);
+        let results = [];
+        let completed = 0;
+
+        promises.forEach((promise, index) => {
+            promise.then((result) => {
+                results[index] = result;
+                completed++;
+
+                if (completed === promises.length) {
+                    resolve(results);
+                }
+            }).catch((error) => {
+                reject(error);
+            });
         });
-      });
     });
-  }
-  const promise1 = Promise.resolve(1);
-const promise2 = Promise.resolve(2);
-const promise3 = Promise.resolve(3);
+}
+const promise1 = Promise.resolve('promise1');
+const promise2 = Promise.resolve('promise2');
+const promise3 = Promise.resolve('promise3');
 
 promiseAll([promise1, promise2, promise3])
-  .then((values) => {
-    console.log(values); // Output: [1, 2, 3]
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+    .then((values) => {
+        console.log(values); 
+    })
+    .catch((error) => {
+        console.error(error);
+    });
